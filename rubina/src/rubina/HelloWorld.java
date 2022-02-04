4c6044573a609f2f60550ec11535bdb69c0eef0f
ucrub <ucrub@LAPTOP-LJOK1JON>
2022-02-04 20:43:11 +0630

    Let me start my first commit

---
+++ rubina/src/rubina/HelloWorld.java
@@ -0,0 +1,52 @@
+package rubina;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Welcome to java.....");
+		
+		
+		                          //PRIMITIVE DATATYPE
+		//NUMBERS:INTEGERS TYPE
+		
+			long distance=9223372036854775807l;//64 bit
+			long distancen=-9223372036854775808L;
+			
+			int weight=2147483647;//32 bit
+			int weightn=-2147483648;
+			
+			short pen=32767;//16 bit
+			short penn=-32768;
+			
+			byte me=127;//8 bit
+			byte men=-128;
+		
+			
+		//FLOATING TYPE
+			double cat=12345678910.127873; //64 bit
+			float catty=1234567.34567f; //32 bit
+			
+			
+		//CHARACTER TYPE
+			//should be written in single cotes like 'c'
+			//can give number as integers too
+			
+			char c='w';//16bit
+			char cc=65535;
+			//char ccc=-65535; u can't have negative value for char
+			
+			
+		//BOOLEAN TYPE
+			boolean boo=true;;
+			boo=false;
+			
+			
+			
+		//STRING TYPE
+			String str="today is java class";//the size of the string is unlimitted. it can contain
+			                                 //words, sentances, paragraph or essaysetc.,
+					
+	}
+
+}
+
+
---
+++ rubina/src/rubina/ValidVariableNames.java
@@ -0,0 +1,21 @@
+package rubina;
+
+public class ValidVariableNames {
+	
+		//VALID NAMES OR IDENTIFIERS
+
+int height;    //names should start with an alphabet or underscore or dollar symbol
+int _height;
+int $height;
+int heigh2t;   //it can have numbers in between or at the end
+int height2;
+
+
+//int 2height;//should not start with a number
+//int hei ght;//should not have space in between
+//int %height;//cannot start with anyother special characters
+//int class;  //names cannnot be a keywords
+
+//package, public, class, int all are packages. There are 32 reserved words in java.
+//the words which is in voilet are the keywords 
+}
